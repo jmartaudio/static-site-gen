b0a7b7c81eeec56ce48eb0e4acf0d1dbd3ee1976
Jason Martinez <jason@bigpictureranch.com>
2025-06-03 10:17:51 -0700

    working on split_nodes_delimiter functon

--- src/function_workspace.py
+++ src/function_workspace.py
@@ -1,3 +1,4 @@
+from warnings import warn
 from textnode import TextNode, TextType
 from htmlnode import LeafNode
 
@@ -24,4 +25,18 @@ def text_node_to_html_node(text_node):
         raise Exception("Unknown text type")
 
 
-
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type.value != "text":
+            return node
+        else:
+            parts = node.text.split(delimiter)
+            if len(parts) != 3:
+                raise Exception("invalid md syntax")
+            else:
+                new_node1 = TextNode(parts[0], node.text_type)
+                new_node2 = TextNode(parts[1], text_type)
+                new_node3 = TextNode(parts[2], node.text_type)
+                new_nodes.extend([new_node1, new_node2, new_node3])
+    return new_nodes
