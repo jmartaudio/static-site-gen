4daf2dda3bdc50160f5661f2cc34f302be1e35b4
Jason Martinez <jason@bigpictureranch.com>
2025-06-12 10:09:35 -0700

    working on generating sub-pages

--- src/main.py
+++ src/main.py
@@ -1,19 +1,30 @@
 import os
-from textnode import TextNode, TextType
-from os_interact import delete_public, copy_source
+import shutil
+from os_interact import copy_files_recursive, generate_page, generate_sub_pages, get_content, makedirs_for_content
+
+dir_path_static = "./static"
+dir_path_public = "./public"
+dir_path_content = "./content"
+template_path = "./template.html"
 
 def main():
-    src = './static'
-    dest = 'public'
+    print("Deleting public directory...")
+    if os.path.exists(dir_path_public):
+        shutil.rmtree(dir_path_public)
 
-    if not os.path.exists(dest):
-        os.makedirs(dest)
+    print("Copying static files to public directory...")
+    copy_files_recursive(dir_path_static, dir_path_public)
+    
+    files_to_make = get_content(dir_path_content)
+    makedirs_for_content(files_to_make, dir_path_content, dir_path_public)
+    generate_sub_pages(files_to_make, dir_path_content, template_path, dir_path_public)
+    print("Generating sub-pages")
 
-    delete_public(dest)
-    copy_source(src, dest)
-
-
-    node = TextNode("Hello! are you working?", TextType.BOLD)
-    print(node)
-
+    print("Generating page...")
+    generate_page(
+        os.path.join(dir_path_content, "index.md"),
+        template_path,
+        os.path.join(dir_path_public, "index.html"),
+    )
+    
 main()
--- src/os_interact.py
+++ src/os_interact.py
@@ -1,24 +1,58 @@
 import os
 import shutil
+from markdown_to_htmlnode import markdown_to_html_node, extract_title
 
-def delete_public(dir):
-    for file in os.listdir(dir):
-        path = os.path.join(dir, file)
-        try:
-            if os.path.isfile(path) or os.path.islink(path):
-                os.unlink(path)
-            elif os.path.isdir(path):
-                shutil.rmtree(path)
-        except Exception as e:
-            print(f'Failed to delete {path}. Reason: {e}')
+def get_content(dir_path_content):
+    all_content = []
+    for item in os.listdir(dir_path_content):
+        all_content.append(item)
+    print(all_content)
 
-def copy_source(src, dest):
-    for item in os.listdir(src):
-        src_item = os.path.join(src, item)
-        dest_item = os.path.join(dest, item)
-        if os.path.isdir(src_item):
-            if not os.path.exists(dest_item):
-                os.makedirs(dest_item)
-            copy_source(src_item, dest_item)
+def makedirs_for_content(all_content, dir_path_content, dir_path_public):
+    for item in all_content:
+        if os.path.isdir(item):
+            dir_path = item.replace(str(dir_path_content), str(dir_path_public))
+            os.makedirs(dir_path)
+
+def generate_sub_pages(all_content, dir_path_content, template_path, dir_path_public):
+    for item in all_content:
+        if os.path.isfile(item):
+            from_path = dir_path_content + item
+            dest_path = dir_path_public + item
+            generate_page(from_path, template_path, dest_path)
+
+def copy_files_recursive(source_dir_path, dest_dir_path):
+    if not os.path.exists(dest_dir_path):
+        os.mkdir(dest_dir_path)
+
+    for filename in os.listdir(source_dir_path):
+        from_path = os.path.join(source_dir_path, filename)
+        dest_path = os.path.join(dest_dir_path, filename)
+        print(f" * {from_path} -> {dest_path}")
+        if os.path.isfile(from_path):
+            shutil.copy(from_path, dest_path)
         else:
-            shutil.copy2(src_item, dest_item)
+            copy_files_recursive(from_path, dest_path)
+
+def generate_page(from_path, template_path, dest_path):
+    print(f" * {from_path} {template_path} -> {dest_path}")
+    from_file = open(from_path, "r")
+    markdown_content = from_file.read()
+    from_file.close()
+
+    template_file = open(template_path, "r")
+    template = template_file.read()
+    template_file.close()
+
+    node = markdown_to_html_node(markdown_content)
+    html = node.to_html()
+
+    title = extract_title(markdown_content)
+    template = template.replace("{{ Title }}", str(title))
+    template = template.replace("{{ Content }}", html)
+
+    dest_dir_path = os.path.dirname(dest_path)
+    if dest_dir_path != "":
+        os.makedirs(dest_dir_path, exist_ok=True)
+    to_file = open(dest_path, "w")
+    to_file.write(template)
